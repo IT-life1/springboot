0373242d02b4ffd1c732c59d052eb5d98ee470fa
sanya <sanya.mozgov@inbox.ru>
2025-06-26 23:24:27 +0300

    model mapper

--- accessing-data-mysql/accessing-data-mysql/src/main/java/com/example/accessing_data_mysql/service/impl/UserServiceIml.java
+++ accessing-data-mysql/accessing-data-mysql/src/main/java/com/example/accessing_data_mysql/service/impl/UserServiceIml.java
@@ -6,6 +6,7 @@ import com.example.accessing_data_mysql.mapper.UserMapper;
 import com.example.accessing_data_mysql.repository.UserInterface;
 import com.example.accessing_data_mysql.service.UserService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,16 +18,20 @@ import java.util.stream.Collectors;
 public class UserServiceIml implements UserService {
 
     private UserInterface userRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
         // Convert UserDto to JPA Entity
-        User user = UserMapper.mapToEntity(userDto);
+        // User user = UserMapper.mapToEntity(userDto);
+        User user = modelMapper.map(userDto, User.class);
 
         User savedUser = userRepository.save(user);
 
         // Convert JPA Entity to UserDto
-        UserDto savedUserDto = UserMapper.mapToDto(savedUser);
+        // UserDto savedUserDto = UserMapper.mapToDto(savedUser);
+
+        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
 
         return savedUserDto;
     }
@@ -35,13 +40,16 @@ public class UserServiceIml implements UserService {
     public UserDto findUserById(long id) {
         Optional<User> optionalUser = userRepository.findById(id);
         User user = optionalUser.get();
-        return UserMapper.mapToDto(user);
+        // return UserMapper.mapToDto(user);
+        return modelMapper.map(user, UserDto.class);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
         List<User> users = userRepository.findAll();
-        return users.stream().map(UserMapper::mapToDto)
+        // return users.stream().map(UserMapper::mapToDto)
+        //        .collect(Collectors.toList());
+        return users.stream().map(user -> modelMapper.map(user, UserDto.class))
                 .collect(Collectors.toList());
     }
 
@@ -51,13 +59,12 @@ public class UserServiceIml implements UserService {
         existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setEmail(user.getEmail());
-        return UserMapper.mapToDto(userRepository.save(existingUser));
+        // return UserMapper.mapToDto(userRepository.save(existingUser));
+        return modelMapper.map(userRepository.save(existingUser), UserDto.class);
     }
 
     @Override
     public void deleteUserById(long id) {
         userRepository.deleteById(id);
     }
-
-
 }
